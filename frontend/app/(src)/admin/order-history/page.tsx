3a4ee5b165bb6c3898e6935cea820aa172be5f82
Lorenzo Parra <alejandro36036@gmail.com>
2026-02-10 17:08:00 -0400

    feature: creation of view history
    creation of view history

---
+++ frontend/app/(src)/admin/order-history/page.tsx
@@ -0,0 +1,9 @@
+const SimpleAdmin = () => {
+  return (
+    <div className="w-full min-h-screen bg-gray-50 font-sans">
+      <main className="max-w-5xl mx-auto p-6"></main>
+    </div>
+  );
+};
+
+export default SimpleAdmin;
