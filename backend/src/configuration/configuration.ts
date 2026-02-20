c8b344ec2512789d55080704ea6a44d49add1aea
Lorenzo Parra <alejandro36036@gmail.com>
2026-02-19 22:22:57 -0400

    feature; abjust of configuration database
    abjust of configuration database

--- backend/src/configuration/configuration.ts
+++ backend/src/configuration/configuration.ts
@@ -3,10 +3,12 @@ import { registerAs } from '@nestjs/config';
 export const dataBaseConfig = registerAs('database', () => ({
   type: 'postgres' as const,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORTa,
-  username: process.env.DB_USER,
+  port: parseInt(process.env.DB_PORT || '6543', 10),
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME,
   synchronize: true,
   autoLoadEntities: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 }));
